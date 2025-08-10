82e58e59bca5b51fa0f297fea13a8b942d9dcb76
GlasgowGracie <2078730w@student.gla.ac.uk>
2025-08-10 13:29:26 +0100

    loading user data from json file successfully

--- server.js
+++ server.js
@@ -1,13 +1,43 @@
 
 var express = require("express");
+var fs = require('fs')
+var bodyparser = require('body-parser');
 var app = express();
+const port = 8000;
+app.use(bodyparser.urlencoded({ extended: true }));
+app.use(bodyparser.json());
+var file = fs.readFileSync("dummyUsers.json"); //get the data from the database into the server
+var data = JSON.parse(file);//holds all of the data in the file
 
 app.get("/", (req, res) => {
   //  res.send("Hello world");
-    var dummyData = {
+    var dummyUserData = {
         "userID": "user1",
-        "username": "user1"
+        "username": "user1",
+        "lives": 2,
+        "score": 150,
     };
-    res.json(dummyData);
+    res.json(dummyUserData);
 });
-app.listen(8000);
+app.listen(port);
+
+
+app.post('/login', (req,res)=>
+{
+console.log("attempting login");
+  const { username, password } = req.body;
+  let success = false;
+  data.users.forEach(user => {
+    if(user.username === username && user.password === password)
+    {
+      success = true;
+      // res.send('success');
+      res.json({ status: "success", username: user.username, gameInProgress:user.gameInProgress,
+        scene:user.scene, musicOn:user.musicOn, volume: user.volume, lives:user.lives, score:user.score
+       });
+    }
+  }) 
+  
+  if(!success) res.json({ status: "fail" });//res.send('fail');
+
+})
